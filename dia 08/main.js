64712589e8d51faafd50dca63fc9bc1f7f6ab361
Fede <efesjotaseles2@gmail.com>
2022-10-26 15:25:15 -0300

    PairProgramming dia8 initial push

---
+++ dia 08/main.js
@@ -0,0 +1,94 @@
+const currentLog = document.getElementById("currentLog");
+
+let agenda = [];
+
+class Contacto{
+  dni;
+  nombre;
+  apellido;
+  telefono;
+
+  constructor(dni, nombre, apellido, telefono){
+    this.dni = dni;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.telefono = telefono;
+  }
+}
+
+let agregarContacto = (dni, nombre, apellido, telefono) => {
+  let contactoNuevo = [dni, nombre, apellido, telefono];
+  agenda.push(contactoNuevo);
+};
+
+let eliminarPorDNI = (dni) => {
+  let resultado = false;
+  let pos = buscarContacto(dni);
+  if (pos !== null) {
+    agenda.splice(pos, 1);
+    resultado = true;
+  }
+  return resultado;
+};
+
+let crearContacto = () => {
+  let DNI = prompt("Ingrese DNI");
+  let nombre = prompt("Ingrese nombre");
+  let apellido = prompt("Ingrese apellido");
+  let tel = prompt("Ingresar télefeno");
+  agregarContacto(DNI, nombre, apellido, tel);
+};
+
+let buscarContacto = (DNI) => {
+  let int = null;
+  agenda.forEach((cont, i) => {
+    if (cont[0] === DNI) {
+      int = i;
+    }
+  });
+  return int;
+};
+
+let mostrarAgenda = () => {
+  if (agenda.length === 0) {
+    alert("Agenda vacía!");
+  } else {
+    agenda.forEach((contacto, i) => {
+      alert(mostrarContacto(contacto));
+    });
+  }
+};
+
+let mostrarContacto = (contacto) => {
+  return `DNI: ${contacto[0]}\nNombre: ${contacto[1]}\nApellido: ${contacto[2]}\nTel: ${contacto[3]}`;
+};
+
+let menu = () => {
+  let op;
+  let DNI;
+  while (op != 0) {
+    op = prompt(
+      "Mostrar agenda: 1 \nCrear contacto: 2 \nEliminar por DNI: 3 \nSalir: 0 \nOpción: "
+    );
+    switch (op) {
+      case "1":
+        mostrarAgenda();
+        //op = 0;
+        break;
+      case "2":
+        crearContacto();
+        break;
+      case "3":
+        DNI = prompt("Ingresar DNI");
+        if (eliminarPorDNI(DNI)) {
+          alert("Contacto elminado!");
+        } else {
+          alert(`No se encontró el contacto indicado\nDNI provisto: ${DNI}`);
+        }
+
+        break;
+      default:
+        break;
+    }
+  }
+};
